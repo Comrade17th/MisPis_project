7f23dd5977e42599043975b29d54a2bcc84468d0
ravshan <ravshannedonat@gmail.com>
2023-03-08 15:52:31 +0300

    login almost done

--- MisPis_WFA/Login.Designer.cs
+++ MisPis_WFA/Login.Designer.cs
@@ -31,37 +31,59 @@
             this.textBox_login = new System.Windows.Forms.TextBox();
             this.textBox_password = new System.Windows.Forms.TextBox();
             this.button_signin = new System.Windows.Forms.Button();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox_login
             // 
-            this.textBox_login.Location = new System.Drawing.Point(109, 148);
+            this.textBox_login.Location = new System.Drawing.Point(107, 87);
             this.textBox_login.Name = "textBox_login";
             this.textBox_login.Size = new System.Drawing.Size(119, 20);
             this.textBox_login.TabIndex = 0;
             // 
             // textBox_password
             // 
-            this.textBox_password.Location = new System.Drawing.Point(109, 184);
+            this.textBox_password.Location = new System.Drawing.Point(107, 123);
             this.textBox_password.Name = "textBox_password";
             this.textBox_password.Size = new System.Drawing.Size(119, 20);
             this.textBox_password.TabIndex = 1;
             // 
             // button_signin
             // 
-            this.button_signin.Location = new System.Drawing.Point(132, 210);
+            this.button_signin.Location = new System.Drawing.Point(107, 149);
             this.button_signin.Name = "button_signin";
-            this.button_signin.Size = new System.Drawing.Size(75, 23);
+            this.button_signin.Size = new System.Drawing.Size(119, 23);
             this.button_signin.TabIndex = 2;
             this.button_signin.Text = "Войти";
             this.button_signin.UseVisualStyleBackColor = true;
             this.button_signin.Click += new System.EventHandler(this.button_signin_Click);
             // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Location = new System.Drawing.Point(104, 71);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(38, 13);
+            this.labelLogin.TabIndex = 3;
+            this.labelLogin.Text = "Логин";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(104, 110);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(45, 13);
+            this.labelPassword.TabIndex = 4;
+            this.labelPassword.Text = "Пароль";
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(337, 360);
+            this.ClientSize = new System.Drawing.Size(325, 261);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelLogin);
             this.Controls.Add(this.button_signin);
             this.Controls.Add(this.textBox_password);
             this.Controls.Add(this.textBox_login);
@@ -78,6 +100,8 @@
         private System.Windows.Forms.TextBox textBox_login;
         private System.Windows.Forms.TextBox textBox_password;
         private System.Windows.Forms.Button button_signin;
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label labelPassword;
     }
 }
 
--- MisPis_WFA/Login.cs
+++ MisPis_WFA/Login.cs
@@ -7,21 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace MisPis_WFA
 {
     public partial class Login : Form
     {
+        database DB = new database();
         string role;
         string login;
         public Login()
         {
             InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
         }
 
         private void Login_Load(object sender, EventArgs e)
         {
-
+            textBox_password.PasswordChar = '*';
+            textBox_login.MaxLength = 50;
+            textBox_password.MaxLength = 50;
         }
 
         private void Open_Temp_Menu()
@@ -31,9 +36,55 @@ namespace MisPis_WFA
             tempMenu.Show();
         }
 
+        private void SignIn()
+        {
+            string login = textBox_login.Text;
+            string password = textBox_password.Text;
+            if (login == "" || password == "")
+            {
+                MessageBox.Show("Пустые поля! Введите логин и пароль");
+            }
+            else
+            {
+                if (IsUserExsists(login, password))
+                {
+                    MessageBox.Show("Вы успешно вошли");
+                    this.login = login;
+                    Open_Temp_Menu();
+                }
+                else
+                {
+                    MessageBox.Show("Не удалось войти, проверьте логин и пароль!");
+                }
+            }
+        }
+
+        private bool IsUserExsists(string login, string password)
+        {
+            bool result = false;
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+            string querryString = $"select * from Users where UserLogin = '{login}' and UserPassword = '{password}'";
+            SqlCommand command = new SqlCommand(querryString, DB.GetConnection());
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+            if (table.Rows.Count == 1)
+                result = true;
+            return result;
+        }
+
         private void button_signin_Click(object sender, EventArgs e)
         {
-            Open_Temp_Menu();
+
+
+            // check is textBoxes Filled
+
+            // make sql select, check password
+            // if user exist && pass good, open next form
+            // else show errMsg
+
+            SignIn();
         }
     }
 }
